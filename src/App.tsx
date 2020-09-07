48b38aec940679cfc69d16a1225829cd8611b038
A'braham Barakhyahu <barakhyahu@gmail.com>
2020-09-07 13:39:20 -0400

    Adds inital structure and implementation

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from "react";
+import "./styles.css";
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import useObservable from './useObservable';
+import randomNameRetriever from './randomNameRetriever';
+import { FromEventTarget } from "rxjs/internal/observable/fromEvent";
+
+const List = ({ items = [], loading = false }) => (
+  <ul className={loading ? 'loading' : undefined}>
+    {items.map(item => (
+      <li key={item}>{item}</li>
+    ))}
+  </ul>
+);
+
+export default function App() {
+  const names: any = useObservable(randomNameRetriever.Subject$);
+  const buttonEl = React.useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const clicky = fromEvent(buttonEl.current as FromEventTarget<HTMLButtonElement> , 'click').pipe(
+      map((event: any)=> { 
+        console.log('in map'); return [event.target.innerHTML]}))
+        .subscribe((e)=>randomNameRetriever.GetNames());
+
+    return () => clicky.unsubscribe();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see something happen!</h2>
+      <List items={names?.data} loading={names?.isLoading} />
+      <button ref={buttonEl}  >More</button>
+    </div>
+    
+  );
+}
---
+++ src/randomNameRetriever.tsx
@@ -0,0 +1,45 @@
+import {ajax} from 'rxjs/ajax';
+import {BehaviorSubject, empty} from 'rxjs';
+import {map, switchMap, startWith} from 'rxjs/operators';
+
+
+//const name$ = of(source);
+const api =(count: any)=> `https://randomuser.me/api/?results=5&seed=rx${count}-react&nat=us&inc=name&noinfo`;
+const getName = (user: any) => `${user.name.first} ${user.name.last}`;
+let count = 0;
+const create = ()=> {
+  const behaviorSubj$ = new BehaviorSubject('More'); 
+const subj = behaviorSubj$.pipe(switchMap(
+  (x: string)=> { 
+    console.log('hey -' + x +'-'); 
+    if(x === "More") {
+      console.log('returning ajax');
+      count+=1;
+      return ajax
+      .getJSON(api(count))
+      .pipe(map((data: any) => {
+        console.log("%o", data.results);
+        return {
+          data:   data.results.map(getName),
+          isLoading: false
+        };
+      }),
+      startWith({
+        data:   [],
+        isLoading: true
+      })); 
+    }
+    return empty();
+  }
+)); 
+  return {
+    Subject$ : subj,
+    GetNames : ()=> {
+      console.log('in more');
+      behaviorSubj$.next('More');
+    }
+  }
+}
+
+export default create();
+ 
